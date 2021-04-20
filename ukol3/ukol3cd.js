4e9f68094176c5121bda3e1fd5b5cf51dd5da438
Petr Vecera <petr.vecera@profiq.com>
2021-04-20 18:49:23 +0200

    Add ukol3

---
+++ ukol3/ukol3cd.js
@@ -0,0 +1,17 @@
+/**
+ * 3c) Pomoci cyklu for vypis cisla 0-4 korme cisla 2 (Napoveda: pouzij if)
+ * 
+ * Vysledek:
+ * 0
+ * 1
+ * 3
+ * 4
+ */
+
+
+/**
+ * 3d) Pomoci extra promene spocitej kolikrat cyklus probehl a po skonceni cyklus
+ * vypis na obrazovku pocet iteraci.
+ * Vysledek:
+ * Cyklus probehl: 5
+ */
